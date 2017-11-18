b730717990330b87504d74526303af5f413d09f1
rahulyadav <rahulyadav44it@gmail.com>
2017-11-18 20:30:10 +0530

    weed  fg fdh

--- NewCandySearch/ViewController.swift
+++ NewCandySearch/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // todays commit
+         todays commit
         // Do any additional setup after loading the view.
     }
 
